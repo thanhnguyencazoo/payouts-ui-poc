df07788d45feb3c18867e686d18566620be5ba45
thanhnguyencazoo <thanh.nguyen@cazoo.co.uk>
2021-04-11 10:14:01 +0100

    shared modules

--- packages/syc-payout-ui/config/webpack.dev.js
+++ packages/syc-payout-ui/config/webpack.dev.js
@@ -18,6 +18,7 @@ const devConfig = {
       exposes: {
         "./SYCPayoutUIApp": "./src/bootstrap",
       },
+      shared: ["react", "react-dom"],
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
